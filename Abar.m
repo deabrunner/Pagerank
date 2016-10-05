86fe462cdf47430a2746ffd4ba0c35c7e027afbe
Dea Brunner <deabrunner@gmail.com>
2016-10-05 17:44:29 +0200

    ny

--- Abar.m
+++ Abar.m
@@ -1,5 +1,25 @@
 function A=Abar(filename)
-A=dlmread (filename);
-%%[4,4]=size(M);
-%%A=zeros(4,4);
+M=dlmread (filename);
+[no_nodes,cc]=size(M);
+A=zeros(no_nodes,no_nodes);
+ for i = 1:no_nodes;
+     for j = 1:cc;
+         
+             h = M(i,j);
+             if h ~= i;
+                 
+                if h > 0; 
+                    A(h,i) = 1;
+                end
+                
+                    
+             end
+      
+        
+     end
+ end
 end
+
+
+
+
--- Amat.m
+++ Amat.m
@@ -1,2 +1,19 @@
-function A=Amat(filename)
-A=dlmread(filename);
+function C=Amat(filename);
+    A = Abar(filename);
+    B = sum(A);
+    A
+    B
+    for i=1:size(B,2);
+        if B(1,i) == 0;
+            B(1,i) = size(B,2)
+            A(1:size(A),i) = 1;
+        end
+    end
+    A
+    
+    C = bsxfun(@rdivide, A, B);
+    C(isnan(C)) = 0;
+    
+    global D
+    D = sum(C);
+end
