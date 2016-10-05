f24c5685527e9b75bbbbe7b876114ee9e8a4e253
Dea Brunner <deabrunner@gmail.com>
2016-10-05 17:47:15 +0200

    deleted spaces

--- Abar.m
+++ Abar.m
@@ -1,23 +1,17 @@
 function A=Abar(filename)
-M=dlmread (filename);
-[no_nodes,cc]=size(M);
-A=zeros(no_nodes,no_nodes);
- for i = 1:no_nodes;
-     for j = 1:cc;
-         
+    M=dlmread (filename);
+    [no_nodes,cc]=size(M);
+    A=zeros(no_nodes,no_nodes);
+    for i = 1:no_nodes;
+        for j = 1:cc;
              h = M(i,j);
              if h ~= i;
-                 
-                if h > 0; 
+                 if h > 0; 
                     A(h,i) = 1;
-                end
-                
-                    
-             end
-      
-        
-     end
- end
+                 end                 
+             end              
+        end
+    end
 end
 
 
